6f457092b2ccd1df67c246e4d87d718e6facffd6
Bahauddinmohammed <61136445+Bahauddinmohammed@users.noreply.github.com>
2022-05-23 17:50:39 +0530

    mj website

---
+++ style.js
@@ -0,0 +1,214 @@
+<div class="abt">ABOUT </div>
+<div class="container">
+<div class="abt-section">
+  <div class="div1">
+    
+    
+  </div>
+ 
+  <div class="div2">
+    <h1 id="abt-logo1">WEB DESIGINER
+      <h1 id="abt-logo2">MOHAMMED BAHAUDDIN</h1>
+    
+    <p class="para"  class="animate__animated animate__slideInUp animate__delay-0.8s">Hi! I'm <span style="font-weight:bold;font-size:24px;font-family:Brush Script Std;color:#197e5c;letter-spacing:1.5px;">Mohammed Bahauddin</span>, from hyderabad, India.<br>I
+      have done my B.Tech in Electrical and Electronic Engineering. Currently working at <span
+        style="color:#016ca7;font-weight:bold;"><a href="https://www.capgemini.com/"
+          style="text-decoration:none;color:#673ab7;">Capgemini</a></span> Technology Services India Limited, as a Senior Software Engineer.  With passion I'm a <span style="color:#ff5722;font-weight:bold;">Full Stack Developer</span>, I have
+      designed my personal website using front-end technologies like <img src="images/icons/html-5.png" style="position:relative;top:10px;">, <img src="images/icons/css.png" style="position:relative;top:10px;">, <img src="images/icons/java-script.png" style="position:relative;top:10px;">, <img src="images/icons/query.png" style="height:32px;position:relative;top:10px;"> and
+      front-end framework like Angular.
+
+      Currently looking to work in challenging environment.<br>I like to figure out problems which are difficult and get
+      a clear conclusion.</p>
+  <span id="hob" style="position:relative;top:10px;">
+    <h3 id="hob">Hobbies:</h3>
+    <p id="hob">=>During my free <img src="images/clock.png" style="position:relative;top:10px;"> I spend my time with other species like <img src="images/pot.png" >, <img src="images/peace.png"style="position:relative;top:5px;" > etc. and beautiful nature.<br>=>spending time without mobile or any other devices. Playing <span><img src="images/chess.png" style="position:relative;top:5px;"></span> doing <img src="images/gymnastics.png" style="position:relative;top:5px;"><br>=><img src="images/gardening1.png">Gardening etc.</p>
+  </span>
+<div class="details">
+  <div id="details">
+    <ul id="list1">
+      <li><span  style="font-size: medium; font-weight: bold;">Birthday:</span><span style="color: #888;">07/12/1998</span></li>
+      <li><span style="font-size: medium; font-weight: bold;">Phone:</span><span style="color: #888;">+91 8247259091</span></li>
+      <li><span style="font-size: medium; font-weight: bold;">Website:</span><span style="color: #888;">mdbahauddin.com</span></li>
+    </ul>
+    <ul id="list2">
+      <li> <span  style="font-size: medium; font-weight: bold;">Degree:</span><span style="color: #888;">B.tech (Electrical and Electronic Engineering)</span></li>
+      <li><span  style="font-size: medium; font-weight: bold;">Mail:</span><span style="color: #888;">mdbahauddin1998@gmail.com</span></li>
+      <li><span  style="font-size: medium; font-weight: bold;">Linkedin:</span><span style="color: #888;"><span>/mdbaha.com</span></li>
+    </ul>
+    <a href="#"><button class="button" type="button" onclick="alert('Hello! Contact@mdbahauddin1998@gmail.com')">Hire Me!</a></button>
+  </div>
+</div>
+</div>
+</div>
+<div class="clear">
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+#abt_div_1,
+#abt_div_2 {
+    background-color: #757575;
+    width: 50%;
+    height: 50%;
+    float: left;
+    border: 1px solid red;
+}
+
+#abt_div1 {
+    width: 80%;
+    height: 50%;
+
+}
+
+.abt {
+    text-align: center;
+    margin: 5rem;
+    font-size: 2.0rem;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: 200;
+
+}
+
+.div1,
+.div2 {
+    float: left;
+    background-color: #fff;
+    width: 60%;
+    height: 50%;
+    float: left;
+
+}
+
+.div1 {
+    background-attachment: fixed;
+}
+
+.div1 {
+    width: 40%;
+    height: 50%;
+    background: linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url(images/bike.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 125vh;
+    background-position: 42% 0%;
+    justify-content: start;
+    flex-direction: column;
+    align-items: center;
+   
+
+
+
+}
+
+#abt-logo1,
+#abt-logo2,
+.para,
+#hob {
+    margin-left: 5%;
+}
+
+#abt-logo1 {
+    margin-left: 5%;
+    display: block;
+    font-family: 'Oswald', sans-serif;
+    font-size: 12px;
+    text-transform: uppercase;
+    color: #F68338;
+    font-weight: 400;
+    letter-spacing: 5px;
+
+
+}
+
+#abt-logo2 {
+    color: #101010;
+    font-family: 'Oswald', sans-serif;
+    margin-bottom: 25px;
+    font-weight: 500;
+    font-size: 30px;
+    letter-spacing: 3px;
+    text-transform: uppercase;
+    line-height: 1.5em;
+    margin-left: 5%;
+    font-weight: bold;
+
+}
+
+.para {
+    font-family: 'Rambla', sans-serif;
+    font-size: 15px;
+    line-height: 3em;
+    font-weight: 400;
+    color: black;
+    margin: 0 0 0px;
+    margin-left: 5%;
+
+}
+
+#hob {
+    font-family: 'Rambla', sans-serif;
+    font-size: 15px;
+    line-height: 2.75em;
+    font-weight: 400;
+    color: black;
+    margin: 0 0 0px;
+    margin-left: 5%;
+
+}
+
+#details ul {
+    margin-left: 5%;
+
+    list-style: none;
+
+}
+
+#details {
+    margin-top: 5%;
+
+}
+
+#details ul li {
+    padding: 1px 0px;
+    font-family: 'Rambla', sans-serif;
+    font-size: 15px;
+    line-height: 1.75em;
+    font-weight: bold;
+
+    margin: 0 0 20px;
+}
+
+#list1,
+#list2 {
+    float: left;
+
+}
+
+#list2 {
+    padding: 0px 70px;
+
+}
+
+.button {
+    background-color: black;
+    border: none;
+    padding: 15px 32px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    margin-left: 5%;
+}
+
+.button a {
+    color: white;
+    text-decoration: none;
+}  */
